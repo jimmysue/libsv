661ec6ae0832c7c8e1ed4f1626dd04994320169b
sjzcv <su.jz@outlook.com>
2018-08-10 12:50:39 +0800

    copy and adjust

--- src/sv_log.h
+++ src/sv_log.h
@@ -1,1 +1,75 @@
+/// codes below were copied from https://github.com/dmcrodrigues/macro-logger
+/// and I make some changes
 
+#ifndef __libsv_sv_log_h__
+#define __libsv_sv_log_h__
+
+#include <time.h>
+#include <string.h>
+
+static inline char *timenow();
+
+#define _FILE strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__
+
+#define NO_LOG          0x00
+#define ERROR_LEVEL     0x01
+#define INFO_LEVEL      0x02
+#define DEBUG_LEVEL     0x03
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL   DEBUG_LEVEL
+#endif
+
+#define PRINTFUNCTION(format, ...)      objc_print(@format, __VA_ARGS__)
+#else
+#define PRINTFUNCTION(format, ...)      fprintf(stderr, format, __VA_ARGS__)
+
+#endif
+
+#define LOG_FMT             "%s | %-7s | %-15s | %s:%d | "
+#define LOG_ARGS(LOG_TAG)   timenow(), LOG_TAG, _FILE, __FUNCTION__, __LINE__
+
+#define NEWLINE     "\n"
+
+#define ERROR_TAG   "ERROR"
+#define INFO_TAG    "INFO"
+#define DEBUG_TAG   "DEBUG"
+
+#if LOG_LEVEL >= DEBUG_LEVEL
+#define LOG_DEBUG(message, args...)     PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(DEBUG_TAG), ## args)
+#else
+#define LOG_DEBUG(message, args...)
+#endif
+
+#if LOG_LEVEL >= INFO_LEVEL
+#define LOG_INFO(message, args...)      PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(INFO_TAG), ## args)
+#else
+#define LOG_INFO(message, args...)
+#endif
+
+#if LOG_LEVEL >= ERROR_LEVEL
+#define LOG_ERROR(message, args...)     PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## args)
+#else
+#define LOG_ERROR(message, args...)
+#endif
+
+#if LOG_LEVEL >= NO_LOGS
+#define LOG_IF_ERROR(condition, message, args...) if (condition) PRINTFUNCTION(LOG_FMT message NEWLINE, LOG_ARGS(ERROR_TAG), ## args)
+#else
+#define LOG_IF_ERROR(condition, message, args...)
+#endif
+
+static inline char *timenow() {
+    static char buffer[64];
+    time_t rawtime;
+    struct tm *timeinfo;
+    
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    
+    strftime(buffer, 64, "%Y-%m-%d %H:%M:%S", timeinfo);
+    
+    return buffer;
+}
+
+#endif // __libsv_sv_log_h__
